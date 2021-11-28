ef0f9aaa730c5a2fd330283aafbf2d90c8f04928
qmigo <ank031100@gmail.com>
2021-11-29 01:50:31 +0530

    first commit

---
+++ js/base.js
@@ -0,0 +1,126 @@
+document.getElementsByTagName("header")[0].innerHTML=`
+<nav class="navbar-main navbar navbar-expand-lg navbar-light bg-light px-4 mb-0">
+<div class="container-fluid">
+  <a href="index.html"><img src="/images/logo.png" alt=""></a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="index.html">Home</a>
+      </li>
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="about-us.html">About Us</a>
+      </li>
+      <li class="nav-item dropdown mx-3">
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false" style="background-color: green; border-radius: 3px; color: #fff;">Products</a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Aromatic Solvent</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">C10 Aromatic Solvent</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">C10 Solvent</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Liquid Paraffin</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Light Diesel Oil</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Heavy Viscous Furnace Oil</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">More Products...</a></li>
+        </ul>
+      </li>
+      <li class="nav-item mx-3">
+        <a class="nav-link" href="contact.html">Contact Us</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>
+`
+document.getElementsByTagName('footer')[0].innerHTML=`
+<div class="footer_one">
+<div class="q_links">
+    <h3>
+        Quick Links
+    </h3>
+    <a href="index.html">Home</a>
+    <a href="about-us.html">About Us</a>
+    <a href="products.html">Products</a>
+    <a href="contact.html">Contact</a>
+    <div class="footerline"></div>
+</div>
+
+<div class="q_links">
+    <h3>
+        Our Products
+    </h3>
+    <a href="products-4.html#96">Aromatic Solvent</a>
+    <a href="products-4.html#107">C10 Aromatic Solvent</a>
+    <a href="products-4.html#83">C10 Solvent</a>
+    <a href="products-5.html#79">Liquid Paraffin</a>
+    <a href="products-5.html#81">Light Diesel Oil</a>
+    <a href="products-5.html#90">Heavy Viscous Furnace Oil</a>
+    <div class="footerline"></div>
+</div>
+
+<div class="q_contact">
+    <h3>
+        Contact Us
+    </h3>
+    <div class="q_contact_link">
+        <i class="fa fa-home"></i>
+        <p>Labdhi Petrochem
+          Plot No. 7911, GIDC Industrial Estate, Near Jitali Chokdi,   GIDC Industrial Estate
+          Ankleshwar, India, 393002</p>
+    </div>
+
+    <div class="q_contact_link">
+        <i class="fa fa-phone"></i>
+        <p>+91-9825317640</p>
+    </div>
+    <div class="q_contact_link">
+        <i class="fa fa-envelope"></i>
+        <a href="https://www.gmail.com" style="text-decoration: underline; cursor: pointer;">labdhi.petrochem@yahoo.in</a>
+    </div>
+    <div class="footerline"></div>
+
+</div>
+
+<div class="q_follow_us">
+    <h3>
+        Follow Us
+    </h3>
+    <div class="q_follow_us_link">
+        <i class="fa fa-facebook"></i>
+        <p>facebook</p>
+    </div>
+
+    <div class="q_follow_us_link">
+        <i class="fa fa-instagram"></i>
+        <p>Instagram</p>
+    </div>
+    <div class="q_follow_us_link">
+        <i class="fa fa-youtube"></i>
+        <p>YouTube</p>
+    </div>
+    <div class="q_follow_us_link">
+        <i class="fa fa-linkedin"></i>
+        <p>Linkedln</p>
+    </div>
+    <div class="footerline"></div>
+
+</div>
+</div>
+<div class="footer_two">
+<div class="footer_content">
+    All Rights Reserved. Labdhi Petrochem (Terms of Use)<br>
+                    <a href="https://adjointindia.com/" style="color: inherit; cursor: pointer;">Powered by AdJoint India</a>
+</div>
+<div class="footer_logo">
+    <img style="cursor:pointer;cursor:hand" src="https://seal.starfieldtech.com/images/3/en/siteseal_sf_3_h_l_m.gif"  alt="SSL site seal - click to verify">
+</div>
+</div>
+`
